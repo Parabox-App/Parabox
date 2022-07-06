352313e21b9c71b94397130413e21c9fac168960
ojhdt <ojhdtmail@gmail.com>
2022-07-06 23:00:42 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/ojhdtapp/parabox/data/local/AppDatabase.kt
+++ app/src/main/java/com/ojhdtapp/parabox/data/local/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.ojhdtapp.parabox.data.local.entity.*
 
-@Database(entities = [ContactEntity::class, MessageEntity::class, ContactMessageCrossRef::class, PluginConnectionEntity::class, ContactPluginConnectionCrossRef::class], version = 1, exportSchema = false)
+@Database(
+    entities = [ContactEntity::class, MessageEntity::class, ContactMessageCrossRef::class, PluginConnectionEntity::class, ContactPluginConnectionCrossRef::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val contactDao: ContactDao
