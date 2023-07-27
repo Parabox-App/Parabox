6888d1eccc8972815a7cc3385759cd17cec9ad43
Ojhdt <ojhdtmail@gmail.com>
2023-07-27 18:40:02 +0800

    feat:search result page

---
+++ app/src/main/java/com/ojhdtapp/parabox/core/util/FormUtil.kt
@@ -0,0 +1,101 @@
+package com.ojhdtapp.parabox.core.util
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.TextUnit
+
+object FormUtil {
+    fun splitPerSpaceOrNewLine(str: String): List<String> = str.split("\\s|([\\r\\n]+)".toRegex())
+    fun splitTwoSpacesOrNewLine(str: String): List<String> = str.split("\\s{2}|([\\r\\n]+)".toRegex())
+    fun splitNewLine(str: String): List<String> = str.split("[\\r\\n]+".toRegex())
+    fun checkTagMinimumCharacter(str: String): Boolean = str.length >= 2
+    fun checkTagMaximumCharacter(str: String): Boolean = str.length < 50
+}
+
+fun String.splitKeeping(str: String): List<String> {
+    return this.split(str).flatMap { listOf(it, str) }.dropLast(1).filterNot { it.isEmpty() }
+}
+
+fun String.ellipsis(maxLength: Int, endKeeping: Int = 3): String {
+    return if (this.length <= maxLength) this
+    else {
+        this.substring(0, maxLength - endKeeping - 1) + "…" + this.substring(this.length - endKeeping, this.length)
+    }
+}
+
+fun String.getAscllString(): String {
+    var sum = ""
+    for (i in this.indices) {
+        sum += this[i].code.toLong()
+    }
+    return sum
+}
+
+@Composable
+fun HyperlinkText(
+    modifier: Modifier = Modifier,
+    fullText: String,
+    hyperLinks: Map<String, String>,
+    textStyle: TextStyle = TextStyle.Default,
+    linkTextColor: Color = Color.Blue,
+    linkTextFontWeight: FontWeight = FontWeight.Normal,
+    linkTextDecoration: TextDecoration = TextDecoration.None,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    textColor: Color = Color.Black
+) {
+    val annotatedString = buildAnnotatedString {
+        append(fullText)
+        addStyle(
+            style = SpanStyle(
+                fontSize = fontSize,
+                color = textColor
+            ),
+            start = 0,
+            end = fullText.length
+        )
+        for((key, value) in hyperLinks){
+
+            val startIndex = fullText.indexOf(key)
+            val endIndex = startIndex + key.length
+            addStyle(
+                style = SpanStyle(
+                    color = linkTextColor,
+                    fontSize = fontSize,
+                    fontWeight = linkTextFontWeight,
+                    textDecoration = linkTextDecoration
+                ),
+                start = startIndex,
+                end = endIndex
+            )
+            addStringAnnotation(
+                tag = "URL",
+                annotation = value,
+                start = startIndex,
+                end = endIndex
+            )
+        }
+    }
+
+    val uriHandler = LocalUriHandler.current
+
+    ClickableText(
+        modifier = modifier,
+        text = annotatedString,
+        style = textStyle,
+        onClick = {
+            annotatedString
+                .getStringAnnotations("URL", it, it)
+                .firstOrNull()?.let { stringAnnotation ->
+                    uriHandler.openUri(stringAnnotation.item)
+                }
+        }
+    )
+}
--- app/src/main/java/com/ojhdtapp/parabox/ui/common/SearchContentType.kt
+++ app/src/main/java/com/ojhdtapp/parabox/ui/common/SearchContentType.kt
@@ -2,11 +2,37 @@ package com.ojhdtapp.parabox.ui.common
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
 import androidx.compose.material.TabRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
@@ -15,9 +41,27 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.material3.fade
+import com.google.accompanist.placeholder.placeholder
 import com.ojhdtapp.parabox.R
+import com.ojhdtapp.parabox.core.util.AvatarUtil
+import com.ojhdtapp.parabox.core.util.splitKeeping
 import com.ojhdtapp.parabox.ui.MainSharedEvent
 import com.ojhdtapp.parabox.ui.MainSharedState
 import kotlinx.coroutines.launch
@@ -36,7 +80,7 @@ fun SearchContent(modifier: Modifier = Modifier, state: MainSharedState, onEvent
     Crossfade(targetState = searchPageState, modifier = modifier) {
         when (it) {
             SearchContentType.RECENT -> {
-                RecentSearchContent()
+                RecentSearchContent(state = state.search, onEvent = onEvent)
             }
 
             SearchContentType.TYPING -> {
@@ -54,11 +98,170 @@ enum class SearchContentType {
     RECENT, TYPING, DONE
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RecentSearchContent(modifier: Modifier = Modifier) {
-    LazyColumn(){
-        item{
-            Text(text = "近期搜索", style = MaterialTheme.typography.labelMedium, color = MaterialTheme.colorScheme.primary)
+fun RecentSearchContent(
+    modifier: Modifier = Modifier,
+    state: MainSharedState.Search,
+    onEvent: (e: MainSharedEvent) -> Unit
+) {
+    LazyColumn() {
+        item {
+            if (state.recentQuery.isNotEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "近期搜索",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+        item {
+            LazyRow(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                items(items = state.recentQuery) {
+                    InputChip(
+                        selected = false,
+                        onClick = { onEvent(MainSharedEvent.QueryInput(it.value)) },
+                        label = { Text(text = it.value) },
+                        trailingIcon = {
+                            IconButton(onClick = { onEvent(MainSharedEvent.DeleteRecentQuery(it.id)) }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Cancel,
+                                    contentDescription = "delete recent query"
+                                )
+                            }
+                        })
+                }
+            }
+        }
+        item {
+            if (state.chat.result.isNotEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "会话",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+        item {
+            Surface(shape = RoundedCornerShape(24.dp), color = MaterialTheme.colorScheme.surface, elevation = 3.dp) {
+                Column() {
+                    state.chat.result.take(6).forEach {
+                        SearchResultItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            avatarModel = it.avatar,
+                            title = buildAnnotatedString {
+                                it.name.splitKeeping(state.query).forEach {
+                                    if (it == state.query) {
+                                        withStyle(
+                                            style = SpanStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                color = MaterialTheme.colorScheme.primary
+                                            )
+                                        ) {
+                                            append(it)
+                                        }
+                                    } else {
+                                        append(it)
+                                    }
+                                }
+                            },
+                            subTitle = null
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
+        item {
+            if (state.message.result.isNotEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "消息",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+        item {
+            Surface(shape = RoundedCornerShape(24.dp), color = MaterialTheme.colorScheme.surface, elevation = 3.dp) {
+                Column() {
+                    state.message.result.take(6).forEach {
+                        SearchResultItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            avatarModel = it.chat!!.avatar,
+                            title = buildAnnotatedString {
+                                it.chat.name.splitKeeping(state.query).forEach {
+                                    if (it == state.query) {
+                                        withStyle(
+                                            style = SpanStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                color = MaterialTheme.colorScheme.primary
+                                            )
+                                        ) {
+                                            append(it)
+                                        }
+                                    } else {
+                                        append(it)
+                                    }
+                                }
+                            },
+                            subTitle = buildAnnotatedString {
+                                append(it.contact!!.name)
+                                append(": ")
+                                it.message.contentString.splitKeeping(state.query).forEach {
+                                    if (it == state.query) {
+                                        withStyle(
+                                            style = SpanStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                color = MaterialTheme.colorScheme.primary
+                                            )
+                                        ) {
+                                            append(it)
+                                        }
+                                    } else {
+                                        append(it)
+                                    }
+                                }
+                            },
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
+        item {
+            if (state.contact.result.isNotEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "联系人",
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
         }
     }
 }
@@ -89,6 +292,77 @@ fun DoneSearchContent(modifier: Modifier = Modifier, state: MainSharedState.Sear
         }
         HorizontalPager(state = pagerState) {
 
+        }
+    }
+}
+
+@Composable
+fun SearchResultItem(
+    modifier: Modifier = Modifier,
+    avatarModel: Any,
+    title: AnnotatedString,
+    subTitle: AnnotatedString?,
+    onClick: () -> Unit,
+) {
+    Row(modifier = modifier.clickable {
+        onClick()
+    }, verticalAlignment = Alignment.CenterVertically) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(40.dp)
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ) {
+            SubcomposeAsyncImage(
+                model = avatarModel,
+                contentDescription = "chat_avatar",
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                val state = painter.state
+                val namedAvatarBm =
+                    AvatarUtil.createNamedAvatarBm(
+                        backgroundColor = MaterialTheme.colorScheme.secondaryContainer.toArgb(),
+                        textColor = MaterialTheme.colorScheme.onSecondaryContainer.toArgb(),
+                        name = title.text
+                    ).asImageBitmap()
+                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .placeholder(
+                                visible = state is AsyncImagePainter.State.Loading,
+                                color = MaterialTheme.colorScheme.secondaryContainer,
+                                highlight = PlaceholderHighlight.fade(),
+                            ),
+                        bitmap = namedAvatarBm,
+                        contentDescription = "named_avatar"
+                    )
+                } else {
+                    SubcomposeAsyncImageContent()
+                }
+            }
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier
+                .weight(1f), verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1
+            )
+            if (subTitle?.isNotBlank() == true) {
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = subTitle,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    maxLines = 1
+                )
+            }
         }
     }
 }
